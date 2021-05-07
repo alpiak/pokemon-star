13268c9c6d7aa278e4cc23135f01a959243bef63
alpiak <hang3126@sina.com>
2021-05-07 11:25:58 +0800

    add transition effect when switching from outdoor scene to battle scene

--- assets/scripts/OutdoorCanvas.ts
+++ assets/scripts/OutdoorCanvas.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, director, game } from 'cc';
+import { _decorator, Animation, Camera, Color, Component, Node, Sprite, SpriteFrame, RenderTexture, UITransform, director, game, renderer, view } from 'cc';
 const { ccclass, property } = _decorator;
 
 import Context from './Context';
@@ -6,8 +6,9 @@ import Gender from './libraries/Gender';
 
 @ccclass('OutdoorCanvas')
 export class OutdoorCanvas extends Component {
-    @property({ type: Node })
+    @property(Node)
     private contextNode!: Node;
+
     public start() {
         this.initContext(this.contextNode);
 
@@ -26,7 +27,7 @@ export class OutdoorCanvas extends Component {
         context.player = context.getPlayer("lucas", Gender.Male, [context.getPokemon(25, Gender.Female, 1)]);
     }
     
-    private enterBattle() {
+    private async enterBattle() {
         const context = this.contextNode.getComponent(Context);
 
         if (!context) {
@@ -35,6 +36,65 @@ export class OutdoorCanvas extends Component {
 
         context.enemyPokemon = context.getPokemon(483, Gender.Female, 1);
         director.preloadScene("battle");
+        await this.fadeOut();
         director.loadScene("battle");
+    }
+
+    private async fadeOut() {
+        const maskNode = this.node.getChildByPath("mask");
+        const sprite = maskNode?.getComponent(Sprite);
+
+        if (!sprite) {
+            return;
+        }
+
+        const node = new Node();
+        
+        director.getScene()?.addChild(node);
+        node.layer = this.node.layer;
+        
+        // const camera = node.addComponent(Camera);\
+        
+        // camera.projection = renderer.scene.CameraProjection.ORTHO;
+        // camera.clearColor = Color.GRAY;
+        // camera.visibility = 4294967295;
+
+        // const renderTexture = new RenderTexture();
+
+        // renderTexture.reset({
+        //     width: (maskNode || this.node).getComponent(UITransform)?.width || 960,
+        //     height: (maskNode || this.node).getComponent(UITransform)?.height || 640,
+        //     colorFormat: RenderTexture.PixelFormat.RGBA8888,
+        //     depthStencilFormat: RenderTexture.DepthStencilFormat.DEPTH_24_STENCIL_8
+        // });
+
+        // const spriteFrame = new SpriteFrame();
+        
+        // spriteFrame.reset({
+        //     originalSize: sprite.spriteFrame?.originalSize,
+        //     rect: sprite.spriteFrame?.rect,
+        //     offset: sprite.spriteFrame?.offset,
+        //     isRotate: sprite.spriteFrame?.isRotated(),
+        //     borderTop: sprite.spriteFrame?.insetTop,
+        //     borderLeft: sprite.spriteFrame?.insetLeft,
+        //     borderBottom: sprite.spriteFrame?.insetBottom,
+        //     borderRight: sprite.spriteFrame?.insetRight,
+        // });
+
+        // camera.targetTexture = renderTexture;
+
+        // await new Promise((r) => setTimeout(r, 0));
+
+        // spriteFrame.texture = renderTexture;
+        // sprite.spriteFrame = spriteFrame;
+        sprite.spriteFrame = this.node.getChildByPath("background")?.getComponent(Sprite)?.spriteFrame || null; // TODO: remove
+
+        const animation = this.node.getChildByPath("mask")?.getComponent(Animation);
+
+        animation?.play("sceneFadeOut");
+        
+        await new Promise((r) => setTimeout(r, (animation?.clips?.filter((clip) => clip?.name === "sceneFadeOut")[0]?.duration || 0) * 1000));
+        
+        // camera?.destroy();
     }
 }
